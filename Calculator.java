368d86c32e2a38837e36d360ca21b7bedd2de1be
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-04-18 20:50:50 +0530

    Completed

--- Calculator.java
+++ Calculator.java
@@ -11,5 +11,30 @@ public class Calculator {
     public static double multiply(double a, double b) {
         return a * b;
     }
+    public static double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    
+    public static double square(double a) {
+        return a * a;
+    }
+
+    
+    public static double cube(double a) {
+        return a * a * a;
+    }
+
+    
+    public static double squareRoot(double a) throws ArithmeticException {
+        if (a < 0) {
+            throw new ArithmeticException("Cannot take square root of a negative number");
+        }
+        return Math.sqrt(a);
+    }
+
     
 }
